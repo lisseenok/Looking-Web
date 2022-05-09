6e5980771c497c68ed71e4bf76a8a426dc884b78
lisseenok <45551611+lisseenok@users.noreply.github.com>
2022-05-10 01:45:29 +0300

    feature add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter} from "react-router-dom";
 import Navbar from "./components/Navbar";
+import AppRouter from "./components/AppRouter";
 
 
 const App = () => {
   return (
       <BrowserRouter>
         <Navbar/>
+        <AppRouter/>
       </BrowserRouter>
   );
 };
---
+++ src/components/AppRouter.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Route, Switch, Redirect} from "react-router-dom";
+import {privateRoutes, publicRoutes} from "../routes";
+import {ADD_HOSTEL_ROUTE, LOGIN_ROUTE} from "../utils/const";
+
+const AppRouter = () => {
+    const user = true
+    return user ?
+        (
+            <Switch>
+                {privateRoutes.map(({path, Component}) =>
+                    <Route path={path} component={Component} exact={true} />
+                )}
+                <Redirect to={ADD_HOSTEL_ROUTE}/>
+            </Switch>
+        )
+        :
+        (
+            <Switch>
+                {publicRoutes.map(({path, Component}) =>
+                    <Route path={path} component={Component} exact={true} />
+                )}
+                <Redirect to={LOGIN_ROUTE}/>
+            </Switch>
+        )
+};
+
+export default AppRouter;
